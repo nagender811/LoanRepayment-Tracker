ec4d0d6a789e9d0b7628d71a454885485972574e
Nagender kumar <kumarnagender811@gmail.com>
2026-07-22 16:38:18 +0530

    Add initial implementation of Loan Repayment Tracker

---
+++ src/LoanRepaymentTracker.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LoanRepaymentTracker  {
+
+}
